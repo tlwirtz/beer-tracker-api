3400676ffb2a742067e64ef5bde27e28c0c22a02
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-09-03 12:58:51 -0700

    modified the env file

--- env.sh
+++ env.sh
@@ -1,4 +1,3 @@
 export DEBUG=beerTracker:*
-export APP_SECRET=donttellanyone
 export PORT=3000
-export MONOGDB_URI=mongodb://localhost/brewBuddy
+export MONOGDB_URI=mongodb://localhost/beerTrackerAPI
