2379b6cd6f46fdbd1624d95736b2acfccb122ee5
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2018-06-04 21:01:47 -0700

    support for transactions

--- model/beer.js
+++ model/beer.js
@@ -1,30 +1,49 @@
 const Joi = require('joi');
 const db = require('./db-admin');
+const httpErrors = require('http-errors');
 
 const beerSchema = Joi.object().keys({
   name: Joi.string().required(),
   device: Joi.object(),
 });
 
+const transactionSchema = Joi.object().keys({
+  type: Joi.string()
+    .valid(['adjust-up', 'adjust-down'])
+    .required(),
+  qty: Joi.number().required(),
+});
+
 const verifySchema = schema => testObj => Joi.validate(testObj, schema);
 const verifyBeerSchema = verifySchema(beerSchema);
+const verifyTransactionSchema = verifySchema(transactionSchema);
 
-const mapIdToSnap = snap => {
+const mapIdToSnap = async snap => {
   let results = [];
   snap.forEach(doc => results.push({ ...doc.data(), id: doc.id }));
   return results;
 };
 
-/**
- * These are the current methods used by the beer-controller.
- * Try to implement as close to possible, but might need some consolidation.
- *
- * These should all return promises.
- * Also, it might be beneficial to create a common interface with Firebase.
- * Device and Beer share the same instances, so there shoudl be a common interface.
- *
- * This would mimic much how the mongo model works, but for Firebase.
- */
+const mapTransactionToBeer = async beers => {
+  return await Promise.all(
+    beers.map(async beer => {
+      const transactions = await fetchTransactions(beer.id);
+      return { ...beer, transactions };
+    })
+  );
+};
+
+const fetchTransactions = async (beerId = null) => {
+  let events = [];
+  await db
+    .collection('beers')
+    .doc(beerId)
+    .collection('transactions')
+    .get()
+    .then(snap => snap.forEach(doc => events.push({ id: doc.id, ...doc.data() })));
+
+  return events;
+};
 
 const save = async beerData => {
   const { error } = verifyBeerSchema(beerData);
@@ -39,12 +58,12 @@ const save = async beerData => {
     .then(ref => findOne(ref.id));
 };
 
-// TODO -- all find methods will also need to get beer transactions and map them to an array
 const find = async () => {
   return db
     .collection('beers')
     .get()
-    .then(mapIdToSnap);
+    .then(mapIdToSnap)
+    .then(mapTransactionToBeer);
 };
 
 const findOne = async id => {
@@ -57,7 +76,8 @@ const findOne = async id => {
     return Promise.reject('Not Found');
   }
 
-  return { id: beer.id, ...beer.data() };
+  const transactions = await fetchTransactions(id);
+  return { id: beer.id, ...beer.data(), transactions };
 };
 
 const findById = async id => {
@@ -83,6 +103,28 @@ const findOneAndUpdate = async (id, update) => {
   return findByIdAndUpdate(id, update);
 };
 
+const addTransaction = async (beerId, transaction) => {
+  const { error } = verifyTransactionSchema(transaction);
+
+  if (error) {
+    return Promise.reject(httpErrors.BadRequest(error));
+  }
+
+  return db
+    .collection('beers')
+    .doc(beerId)
+    .collection('transactions')
+    .add({ ...transaction, time: Date.now() });
+};
+
+const removeTransaction = async (beerId, transactionId) => {
+  return db
+    .collection('beers')
+    .doc(beerId)
+    .collection('transactions')
+    .remove(transactionId);
+};
+
 const beerModel = {
   save,
   findOne,
@@ -91,6 +133,8 @@ const beerModel = {
   remove,
   findById,
   findOneAndUpdate,
+  addTransaction,
+  removeTransaction,
 };
 
 module.exports = beerModel;
